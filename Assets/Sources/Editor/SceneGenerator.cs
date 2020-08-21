a552b189c455562fbfea49d2ad2ba50b607865b6
estupin <estupin@spotware.com>
2020-08-21 17:15:44 +0300

    Introduce SceneGenerator

---
+++ Assets/Sources/Editor/SceneGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Sources.Extensions;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Sources.Editor
+{
+    [InitializeOnLoad]
+    public class SceneGenerator
+    {
+        static SceneGenerator()
+        {
+            EditorSceneManager.newSceneCreated += OnNewSceneCreated;
+        }
+
+        private static void OnNewSceneCreated(Scene scene, NewSceneSetup setup, NewSceneMode mode)
+        {
+            var setupFolder = CreateFolder("Setup");
+            var worldFolder = CreateFolder("World");
+            CreateFolder("UI");
+            var camerasFolder = CreateFolder("Cameras").AttachToParent(setupFolder);
+            CreateFolder("Static").AttachToParent(worldFolder);
+            CreateFolder("Dynamic").AttachToParent(worldFolder);
+
+            if (Camera.main != null)
+                Camera.main.transform.AttachToParent(camerasFolder);
+        }
+
+        private static Transform CreateFolder(string name)
+        {
+            return new GameObject(name).transform;
+        }
+    }
+}
---
+++ Assets/Sources/Extensions/TransformExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Sources.Extensions
+{
+    public static class TransformExtensions
+    {
+        public static Transform AttachToParent(this Transform transform, Transform parent)
+        {
+            transform.parent = parent;
+
+            return transform;
+        }
+    }
+}
